1be636db3b3c40e13974c1ca9417ada6cdf98de8
MichaelBabiy23 <mr.babiymichael@gmail.com>
2024-06-23 13:16:17 +0300

    rename of ex1 to ex2

--- run_me.sh
+++ run_me.sh
@@ -2,7 +2,7 @@
 # 323073734
 
 # Compile the C code
-gcc -Wall alias_manager.c string_utils.c command_executor.c process_handler.c process_manager.c ex1.c -o ex1
+gcc -Wall alias_manager.c string_utils.c command_executor.c process_handler.c process_manager.c ex2.c -o ex2
 
 # Run the compiled program
-./ex1
+./ex2
